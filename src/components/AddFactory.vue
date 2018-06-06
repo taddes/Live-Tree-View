34a43e0914fa1699fbd396fe13a8e11723db0ac7
Taddes Korris <taddes.korris@gmail.com>
2018-06-06 11:42:51 -0400

    created AddFactory component to add factories by a form. Updated router index.js file with this url route as well

---
+++ src/components/AddFactory.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Add Factory</h2>
+</template>
+
+<script>
+export default {
+  name: 'AddFactory',
+  data(){
+    return {
+
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddFactory from '@/components/AddFactory'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-factory',
+      name: 'AddFactory',
+      component: AddFactory
     }
   ]
 })
