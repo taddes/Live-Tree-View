50598518767a6418ca6d24deb244bf8aef0be3fb
taddes <taddes.korris@gmail.com>
2018-06-13 17:25:36 -0400

    attempted changes for deployment

--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Factory = require('./api/models/factoryListModel')
+const Factory = require('./api/models/factoryListModel');
 const path = require('path');
-var serveStatic = require('serve-static');
+const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
-const routes = require('./api/routes/factoryListRoutes')
-const socket = require('socket.io')
-// const cors = require('cors');
+const routes = require('./api/routes/factoryListRoutes');
+const socket = require('socket.io');
+
 
 // Start Express Server
 const app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-// Define Port
+// Define port
 const PORT = process.env.MONGODB_URI || 3000;
 
+// Connect mongoose 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://heroku_24z0cgtp:9k2c8j1ksh0l4s16btlgt773vl@ds257470.mlab.com:57470/heroku_24z0cgtp')
+mongoose.connect('mongodb://heroku_24z0cgtp:9k2c8j1ksh0l4s16btlgt773vl@ds257470.mlab.com:57470/heroku_24z0cgtp');
+
 // Body Parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 routes(app);
 
-// Enable Cors
-// app.use(cors());
-
 // Start Server & Listener
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port:${PORT}`);
 });
 
-// Socket setup
+// Socket.io setup
 const io = socket(server);
 
 io.on('connection', (socket) => {
@@ -41,32 +40,28 @@ io.on('connection', (socket) => {
   socket.on('deleteFactory', (factory) => {
     console.log(`socket deleteFactory event`)
     // Emit this event to all clients connected
-    io.emit('deleteFactory', factory)
+    io.emit('deleteFactory', factory);
     console.log(`socket deleteFactory emission!`)
-  })
+  });
+
 // add factory
 socket.on('addFactory', (factory) => {
   console.log(`socket addFactory event`)
   // Emit this event to all clients connected
-  io.emit('addFactory', factory)
+  io.emit('addFactory', factory);
   console.log(`socket addFactory emission!`)
-})
+});
 
 // edit factory
 socket.on('editFactory', (factory) => {
   console.log(`socket editFactory event`)
   // Emit this event to all clients connected
-  io.emit('editFactory', factory)
+  io.emit('editFactory', factory);
   console.log(`socket editFactory emission!`)
-})
+});
 
-
-  io.emit('user joined', socket.id)
-  // socket.on('message', (data) => {
-  //   console.log(data)
-  //   io.sockets.emit('message', socket.id);
-  // })
   socket.on('disconnect', () => {
-    console.log(`disconnected from socket id: ${socket.id}`)
-  })
+    console.log(`disconnected from socket id: ${socket.id}`);
+  });
+
 });
